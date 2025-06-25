88bfc866ee59cd6a51a3a374c239d7ae8ea30f48
Ricardo Brea <ricardo.brea@unahur.edu.ar>
2025-06-24 22:47:36 -0300

    Remove redundant COMPILATIONDB_PATH environment variable from build job; ensure correct path is set in extra_script.py for SonarQube scan.

--- extra_script.py
+++ extra_script.py
@@ -9,3 +9,10 @@ build_tag = env['PIOENV']
 version_tag = os.getenv("FIRMWARE_VERSION")
 
 env.Replace(PROGNAME="firmware_%s_%s" % (build_tag, version_tag))
+
+# esto es para la el scan con sonarqube
+# include toolchain paths
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+
+# override compilation DB path
+env.Replace(COMPILATIONDB_PATH=os.path.join("$BUILD_WRAPPER_OUT_DIR", "compile_commands.json"))
